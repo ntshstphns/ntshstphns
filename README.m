b8f77a4ef0a044cef196dc6eb328d8d945dcffe1
Natasha Stephens <91845099+ntshstphns@users.noreply.github.com>
2023-05-08 17:00:48 -0400

    Update and rename README.md to README.m

--- README.m
+++ README.m
@@ -18,11 +18,14 @@
 
 
 
-[<img align="left" alt="JoshMadakor | LinkedIn" width="22px" src="https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/linkedin.svg" />][linkedin]
+[<img align="left" alt="natasha-stephens | LinkedIn" width="22px" src="https://cdn.jsdelivr.net/npm/simple-icons@v3/icons/linkedin.svg" />][linkedin]
 
 
 
-[linkedin]:https://www.linkedin.com/in/natasha-stephens-50663018b
+[linkedin]:https://www.linkedin.com/in/
+natasha-stephens-50663018b
+
+
 
 
 
